8234da574bf0891942f9e579be2f4a44647abcdf
Mathues <mmacedo335@gmail.com>
2020-05-26 23:30:53 -0300

    tezinho

--- public/home.js
+++ public/home.js
@@ -1,7 +1,9 @@
 
 
 function habilita_survey(){
-    document.getElementById("balao_questionario").style.display = "block";}
+    document.getElementById("balao_questionario").style.display = "block";
+    document.getElementById("executar_questionario").style.display = "block";
+}
     
 function timer_balao () {
     setTimeout(function() {
@@ -11,3 +13,11 @@ function timer_balao () {
 function fechar_balao(){
     document.getElementById("balao_questionario").style.display = "none";
 }
+function aparecer_texto(){
+    document.getElementById("balao").style.display = "block"
+}
+function reset(){
+    document.getElementById("balao").style.display = "none"
+}
+
+
