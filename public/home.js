22b7eb70d917a2b55fb18d3b44d61b97477af837
Mathues <mmacedo335@gmail.com>
2020-05-27 13:36:43 -0300

    atualização botões

--- public/home.js
+++ public/home.js
@@ -3,6 +3,19 @@
 function habilita_survey(){
     document.getElementById("balao_questionario").style.display = "block";
     document.getElementById("executar_questionario").style.display = "block";
+    document.getElementById("button_informacao").style.opacity = "0.2";
+    document.getElementById("img_questionario").style.opacity = "0.2";
+    document.getElementById("img_questionarioo").style.opacity = "0.2";
+    document.getElementById("balao_questionario").style.opacity = "0.2";
+    document.getElementById("balao").style.opacity = "0.2";
+    
+}
+function close_modal(){
+    document.getElementById("button_informacao").style.opacity = "1";
+    document.getElementById("img_questionario").style.opacity = "1";
+    document.getElementById("img_questionarioo").style.opacity = "1";
+    document.getElementById("balao_questionario").style.opacity = "1";
+    document.getElementById("balao").style.opacity = "1";
 }
     
 function timer_balao () {
