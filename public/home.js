a801c1f7f69a28e02fa29ad10dfcf86c12f874de
Mathues <mmacedo335@gmail.com>
2020-05-26 21:55:11 -0300

    tezinho

--- public/home.js
+++ public/home.js
@@ -1,24 +1,13 @@
-function close_questionario() {
-    document.getElementById("conteudo").style.display = "none";
-}
-function abrir_questionario() {
-    document.getElementById("conteudo").style.display = "block";
-}
-function link_questionario() {
-    document.getElementById("conteudo").style.display = "none";
 
-}
-function close_informacoes() {
-    document.getElementById("conteudo_informacoes").style.display = "none";
-}
-function abrir_informacoes() {
-    document.getElementById("conteudo_informacoes").style.display = "block";
-}
-function close_questionario_mobile() {
-    document.getElementById("balao").style.display = "none";
-    
-}
+
 function habilita_survey(){
-    document.getElementById("questionario").style.display = "block";
-
+    document.getElementById("balao_questionario").style.display = "block";}
+    
+function timer_balao () {
+    setTimeout(function() {
+        document.getElementById('balao').style.display = "none";;
+        }, 4000);
+      }
+function fechar_balao(){
+    document.getElementById("balao_questionario").style.display = "none";
 }
